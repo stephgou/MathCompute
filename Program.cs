c0146b4cc5b7fe574cf229fad41bd87e315a48ac
Stéphane Goudeau <stephgou@microsoft.com>
2020-11-20 16:12:37 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MathCompute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	        Stopwatch timePerCompute;
+            double result = 0;
+	        int inputNum = 25;   
+
+            Console.WriteLine("Starting MathCompute");
+            while(true) {
+                timePerCompute = Stopwatch.StartNew();
+                for (var i = Math.Pow(inputNum, 6); i >= 0; i--) {    
+                    result += Math.Atan(i) * Math.Tan(i);
+                };            
+                timePerCompute.Stop();
+                Console.WriteLine("Elapsed milliseconds : {0}ms", timePerCompute.ElapsedMilliseconds);
+            }
+        }
+    }
+}
